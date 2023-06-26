af72bbcd60827494aead9b5783e1befa76a9c436
pietrodev07 <pietro.dev.07@gmail.com>
2023-06-26 11:32:10 +0200

    feat: add urls of api

---
+++ src/services/url.js
@@ -0,0 +1,5 @@
+const ISS_POSITION_URL = "http://api.open-notify.org/iss-now.json";
+
+const PEOPLE_SPACE_URL = "http://api.open-notify.org/astros.json";
+
+export { ISS_POSITION_URL, PEOPLE_SPACE_URL };
