fd09ce6a22ef337561b2a659d1b2a222727f25b1
pietrodev07 <pietro.dev.07@gmail.com>
2023-06-26 12:13:31 +0200

    feat: add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
 import Main from "./components/Main/Main"
 
@@ -10,6 +11,8 @@ const App = () => {
       <Header />
 
       <Main />
+
+      <Footer />
 
     </div>
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import './Footer.css'
+
+const Footer = () => {
+
+  return (
+
+    <footer className="footer">
+
+      <p>Made by PietroDev07</p>
+
+    </footer>
+
+  )
+
+}
+
+export default Footer
